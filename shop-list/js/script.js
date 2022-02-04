a8ddc9b55c98c153b1f5ddf4db387c609413177c
Vladimir <50200804+Tartalon@users.noreply.github.com>
2022-02-04 16:25:26 +0200

    func addNewProd + showAll

--- shop-list/js/script.js
+++ shop-list/js/script.js
@@ -11,16 +11,37 @@ const data = [
 	{ name: 'bread', quantity: 1, bought: false, id: 3 },
 ];
 
-const maxId = 4;
+let maxId = 4;
 
-const products = [...data];
+let products = [...data];
+let newProducts = [];
 
 allButton.addEventListener('click', () => {
-	showAllProducts(products, productsList);
+	showAllProducts(products, newProducts, productsList);
 });
 
-function showAllProducts(arr, element) {
-	arr.forEach(item => {
+addButton.addEventListener('click', e => {
+	e.preventDefault();
+	addNewProduct(productInput, productsList);
+});
+
+function showAllProducts(dataArr, newProdArr, element) {
+	let allProducts = [];
+
+	if (newProdArr.length) {
+		allProducts = [...newProdArr, ...dataArr];
+	} else {
+		allProducts = [...dataArr];
+	}
+
+	if (productsList.childElementCount) {
+		const visibleProd = document.querySelectorAll('.product__item');
+		if (visibleProd.length === allProducts.length) {
+			return;
+		}
+	}
+
+	allProducts.forEach(item => {
 		element.insertAdjacentHTML('beforeend', createElement(item));
 	});
 }
@@ -46,8 +67,30 @@ function createElement(obj) {
 	return li;
 }
 
-function changeQuantity(quantity, element) {
-	if (element.value !== quantity) {
-		element.value = quantity;
-	}
+// function changeQuantity(quantity, element) {
+// 	if (element.value !== quantity) {
+// 		element.value = quantity;
+// 	}
+// }
+
+function addNewProduct(input, element) {
+	clearList(productsList);
+	maxId++;
+
+	const productName = input.value.toLowerCase().trim();
+	const newProduct = {
+		name: productName,
+		quantity: 1,
+		bought: false,
+		id: maxId,
+	};
+
+	newProducts.unshift(newProduct);
+	newProducts.forEach(item => {
+		element.insertAdjacentHTML('beforeend', createElement(item));
+	});
+}
+
+function clearList(element) {
+	element.innerHTML = '';
 }
