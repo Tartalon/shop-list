4ae67ae9ffd1d9b5dfe18c20db46256a99600305
Vladimir <50200804+Tartalon@users.noreply.github.com>
2022-02-09 13:20:42 +0200

    func addNewProd changed quantity

--- shop-list/js/script.js
+++ shop-list/js/script.js
@@ -1,136 +1,137 @@
-"use strict";
+'use strict';
 
-const productInput = document.querySelector(".form__input"),
-  addButton = document.querySelector(".btn-add"),
-  allButton = document.querySelector(".btn-all"),
-  productsList = document.querySelector(".products__list");
+const productInput = document.querySelector('.form__input'),
+	addButton = document.querySelector('.btn-add'),
+	allButton = document.querySelector('.btn-all'),
+	productsList = document.querySelector('.products__list');
 
 const data = [
-  { name: "apple", quantity: 1, bought: true, id: 1, visible: false },
-  { name: "potato", quantity: 1, bought: false, id: 2, visible: false },
-  { name: "bread", quantity: 1, bought: true, id: 3, visible: false },
+	{ name: 'apple', quantity: 1, bought: true, id: 1, visible: false },
+	{ name: 'potato', quantity: 1, bought: false, id: 2, visible: false },
+	{ name: 'bread', quantity: 1, bought: true, id: 3, visible: false },
 ];
 
 let maxId = 3;
 
 let products = [...data];
 
-allButton.addEventListener("click", () => {
-  showAllProducts(sortsProducts(products), productsList);
+allButton.addEventListener('click', () => {
+	showAllProducts(sortsProducts(products), productsList);
 });
 
-addButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  if (!productInput.value) return;
-  sortsProducts(products);
-  addNewProduct(productInput);
+addButton.addEventListener('click', e => {
+	e.preventDefault();
+	if (!productInput.value) return;
+
+	addNewProduct(productInput);
+	sortsProducts(products);
+	render(products, productsList);
 });
 
-productsList.addEventListener("click", (e) => {
-  let target = e.target;
-  if (target.classList.contains("product__input")) {
-    const prodName = target.parentNode.textContent.trim();
-    for (const product of products) {
-      if (product.name === prodName) {
-        product.bought = target.checked;
-      }
-    }
-  }
+productsList.addEventListener('click', e => {
+	let target = e.target;
+	if (target.classList.contains('product__input')) {
+		const prodName = target.parentNode.textContent.trim();
+		for (const product of products) {
+			if (product.name === prodName) {
+				product.bought = target.checked;
+			}
+		}
+	}
 });
 
 function showAllProducts(arr, element) {
-  clearList(productsList);
+	clearList(productsList);
 
-  if (productsList.childElementCount) {
-    const visibleProd = document.querySelectorAll(".product__item");
-    if (visibleProd.length === arr.length) {
-      return;
-    }
-  }
+	if (productsList.childElementCount) {
+		const visibleProd = document.querySelectorAll('.product__item');
+		if (visibleProd.length === arr.length) {
+			return;
+		}
+	}
 
-  arr.forEach((item) => {
-    item.visible = true;
-  });
+	arr.forEach(item => {
+		item.visible = true;
+	});
 
-  render(arr, element);
+	render(arr, element);
 }
 
 function createElement(obj) {
-  const li = `
+	const li = `
     <li class="product__item list-group-item d-flex justify-content-between">
       <div class="product__input-wrapper">
         <input
           class="form-check-input me-3 product__input"
           type="checkbox"
           aria-label="buy"
-          ${obj.bought ? "checked" : ""}
+          ${obj.bought ? 'checked' : ''}
         />
         ${obj.name}
       </div>
       <input
 							type="number"
 							class="product__quantity text-center"
-							value="1"
+							value=${obj.quantity}
 						/>
     </li>
   `;
-  return li;
+	return li;
 }
 
 function addNewProduct(input) {
-  clearList(productsList);
-  const productName = input.value.toLowerCase().trim();
-  let arrWithNewProd = [];
+	clearList(productsList);
+	const productName = input.value.toLowerCase().trim();
+	// let copyProducts = products;
+	let sameProduct = false;
 
-  for (let i = 0; i < products.length; i++) {
-    if (products[i].name === productName) {
-      products[i].quantity++;
-    } else {
-      arrWithNewProd.push(products[i]);
-      if (i === products.length - 1) {
-        maxId++;
-        products.push(createNewProduct(productName));
-      }
-    }
-  }
+	for (let i = 0; i < products.length; i++) {
+		if (products[i].name === productName) {
+			products[i].quantity++;
+			sameProduct = true;
+		}
+	}
 
-  sortsProducts(products);
-  render(products, productsList);
-  clearInput(productInput);
+	if (!sameProduct) {
+		maxId++;
+		products.push(createNewProduct(productName));
+	}
+
+	clearInput(productInput);
 }
 
 function createNewProduct(name) {
-  return {
-    name: name,
-    quantity: 1,
-    bought: false,
-    id: maxId,
-    visible: true,
-  };
+	return {
+		name: name,
+		quantity: 1,
+		bought: false,
+		id: maxId,
+		visible: true,
+	};
 }
 
 function render(arr, element) {
-  arr.forEach((item) => {
-    if (item.visible === true) {
-      element.insertAdjacentHTML("beforeend", createElement(item));
-    }
-  });
+	arr.forEach(item => {
+		if (item.visible === true) {
+			element.insertAdjacentHTML('beforeend', createElement(item));
+		}
+	});
 }
 
 function sortsProducts(arr) {
-  let sortedProducts = arr.sort((element) => {
-    if (element.bought < 1) {
-      return -1;
-    }
-  });
+	let sortedProducts = arr.sort(element => {
+		if (element.bought < 1) {
+			return -1;
+		}
+	});
 
-  return sortedProducts;
+	return sortedProducts;
 }
 
 function clearList(ul) {
-  ul.innerHTML = "";
+	ul.innerHTML = '';
 }
 
 function clearInput(input) {
-  input.value = "";
+	input.value = '';
 }
